78b4f1465ee43da7c3c836c02c3209741d0268d9
Jesper Falkenby <jesper.hansson.f@gmail.com>
2026-02-08 23:53:03 +0100

    player respawns if fallen

--- player.gd
+++ player.gd
@@ -3,8 +3,18 @@ extends CharacterBody2D
 @export var speed: float = 200.0
 @export var jump_velocity: float = -350.0
 @export var gravity: float = 900.0
+@export var fall_threshold: float = 500.0  # Y position below which player respawns
+
+var spawn_position: Vector2
+
+func _ready() -> void:
+    spawn_position = global_position
 
 func _physics_process(delta: float) -> void:
+    # Check if player fell off the level
+    if global_position.y > fall_threshold:
+        respawn()
+
     # Apply gravity
     if not is_on_floor():
         velocity.y += gravity * delta
@@ -21,3 +31,7 @@ func _physics_process(delta: float) -> void:
         velocity.x = move_toward(velocity.x, 0, speed)
 
     move_and_slide()
+
+func respawn() -> void:
+    global_position = spawn_position
+    velocity = Vector2.ZERO
